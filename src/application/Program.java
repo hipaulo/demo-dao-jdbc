3d475e66c1297bfda481cd122cde1ae96ac8f381
hipaulo <paulomeloads@gmail.com>
2021-07-26 19:12:51 -0300

    findAll implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,13 @@ public class Program {
 			System.out.println(obj);
 			
 		}
+		
+		System.out.println(" \n==== Test 3: seller findByAll ====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+			
+		}
 
 	}
 
